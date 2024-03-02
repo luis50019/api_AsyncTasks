fd8d1ce4138723aabfaccf587575d1765ec2f4d5
luis50019 <teddiazdiaz019@gmail.com>
2024-03-02 03:35:04 -0600

    configurando variables globales

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const TOKEN_SECRET = 'naturalCasiofx-82ES';
 export const PORT = process.env.PORT || 4000;
-export const URL_DATA_BASE = "mongodb+srv://localhost/taskHub";
-export const FRONTEND_URL = 'http://localhost:5173';
+export const URL_DATA_BASE = process.env.URL_DATA_BASE || "mongodb+srv://localhost/taskHub";
+export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
