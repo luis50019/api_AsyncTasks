35ec660193158dacda35d50fc46406e3f12f615a
luis50019 <teddiazdiaz019@gmail.com>
2024-03-02 22:14:08 -0600

    deploy en vercel

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const TOKEN_SECRET = 'naturalCasiofx-82ES';
 export const PORT = process.env.PORT || 4000;
-export const URL_DATA_BASE = process.env.URL_DATA_BASE || "mongodb+srv://localhost/taskHub";
+export const URL_DATA_BASE = process.env.URL_DATA_BASE || "mongodb+srv://asynctaskweb:BkVsmUt4IwFwFdMi@cluster0.rojizgn.mongodb.net/taskHub";
 export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
