a0593e633dc5812193ca44a129613f2ab9beb8af
LuisDiaz <teddiazdiaz019@gmail.com>
2025-03-14 00:28:39 -0600

    cambios en cors

--- src/app.js
+++ src/app.js
@@ -7,11 +7,12 @@ import authRoutes from './routers/auth.routes.js';
 import taskRouter from './routers/task.routes.js';
 
 const app = express();
-app.use(corsMiddleware());
 
+app.use(corsMiddleware());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(authRoutes);
 app.use(taskRouter);
 
