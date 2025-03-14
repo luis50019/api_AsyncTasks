ea9ef15e65233a75f4effa55d4da723d524b1791
LuisDiaz <teddiazdiaz019@gmail.com>
2025-03-13 19:44:52 -0600

    cambios

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const TOKEN_SECRET = "naturalCasiofx-82ES";
 export const PORT = process.env.PORT || 4000;
 export const URL_DATA_BASE =
-  "mongodb+srv://teddiazdiaz019:<db_password>@clustertasks.w1omw.mongodb.net/?retryWrites=true&w=majority&appName=clusterTasks";
+  "mongodb+srv://teddiazdiaz019:tfO1D9GL1t0IzvCP@clustertasks.w1omw.mongodb.net/?retryWrites=true&w=majority&appName=clusterTasks";
 export const FRONTEND_URL = "https://async-tasks.vercel.app" || "http://localhost:5173";
 
