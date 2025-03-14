3adcc06a8726faa2e994b604cd2095b3cf44a235
LuisDiaz <teddiazdiaz019@gmail.com>
2025-03-13 19:39:29 -0600

    cambios de seguridad

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 export const TOKEN_SECRET = "naturalCasiofx-82ES";
 export const PORT = process.env.PORT || 4000;
-export const URL_DATA_BASE = process.env.URL_DATA_BASE ||"mongodb+srv://teddiazdiaz019:tfO1D9GL1t0IzvCP@clustertasks.w1omw.mongodb.net/";
+export const URL_DATA_BASE =
+  "mongodb+srv://teddiazdiaz019:tfO1D9GL1t0IzvCP@clustertasks.w1omw.mongodb.net/" ||
+  "mongodb+srv://teddiazdiaz019:tfO1D9GL1t0IzvCP@clustertasks.w1omw.mongodb.net/";
 export const FRONTEND_URL = "https://async-tasks.vercel.app" || "http://localhost:5173";
 
