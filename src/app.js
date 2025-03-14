26af21e0d2609f5d67e5fb6faf7b91561492d03e
LuisDiaz <teddiazdiaz019@gmail.com>
2025-03-14 00:34:47 -0600

    cambios en cors

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
-import { corsMiddleware } from './middlewares/cors.js';
+import cors from 'cors';
+import { FRONTEND_URL } from './config.js';
 
 import authRoutes from './routers/auth.routes.js';
 import taskRouter from './routers/task.routes.js';
 
 const app = express();
 
-app.use(corsMiddleware());
+app.use(cors({
+    origin: FRONTEND_URL,
+    credentials: true
+}))
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(authRoutes);
 app.use(taskRouter);
 
