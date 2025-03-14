56e9a7769bd533b22f96a2ef53df5c55afb27e69
LuisDiaz <teddiazdiaz019@gmail.com>
2025-03-13 19:34:21 -0600

    cambiando la url de cors

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const TOKEN_SECRET = "naturalCasiofx-82ES";
 export const PORT = process.env.PORT || 4000;
 export const URL_DATA_BASE = process.env.URL_DATA_BASE ||"mongodb+srv://teddiazdiaz019:tfO1D9GL1t0IzvCP@clustertasks.w1omw.mongodb.net/";
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const FRONTEND_URL = "https://async-tasks.vercel.app" || "http://localhost:5173";
 
