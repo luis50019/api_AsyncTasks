f1792c95c5a2b6a0a91e25bab04aa44d135665d4
LuisDiaz <teddiazdiaz019@gmail.com>
2025-03-14 00:02:44 -0600

    corrigiendo error de jwt

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export const TOKEN_SECRET = "naturalCasiofx-82ES";
 export const PORT = process.env.PORT || 4000;
 export const URL_DATA_BASE =
   "mongodb+srv://teddiazdiaz019:tfO1D9GL1t0IzvCP@clustertasks.w1omw.mongodb.net/?retryWrites=true&w=majority&appName=clusterTasks";
-export const FRONTEND_URL = "http://localhost:5173";
+export const FRONTEND_URL = "https://async-tasks.vercel.app";
 
 // "https://async-tasks.vercel.app"
